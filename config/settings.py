aaa9142c23019c1be7b2443471f91a22cac255b2
Abdulaziz <abdulazizkomilov2001@gmail.com>
2022-03-21 11:58:15 +0500

    adds

--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-fx-m8-xf7j67!b)fznn=_1=w0o(3-c^mkm(nqtw^2$y-&cikh4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
